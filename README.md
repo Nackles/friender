b6e90ee1a0019c51916e1077cb3d4d2e7073ef14
Nackles <45973617+Nackles@users.noreply.github.com>
2019-04-07 21:07:27 -0700

    saving for progress JUST MAKE THE MATCH SHOW!!

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# friender (unfinished)
+# Friender
 
-# TODO: - Currently having a bug in the friends logic of apiRoutes where friends[i] is returning undefined
 # TODO: - I cannot figure out why the modal is not popping up even to test. Is it the jQuery?
-# TODO: - Would be nice for the button at '/' to change the route
 # TODO: - Make a better readme.
 # TODO: - Deploy to Heroku.
-# TODO: - Sleep first..
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
         //friending logic
         //for each friend in friends
         // TODO:
-        console.log("friends length",friends.friends.length)
+        console.log("friends length", friends.friends.length)
         var theMatch = input;
         for (let i = 0; i < friends.friends.length; i++) {
             var checkme = input.scores;
@@ -31,17 +31,16 @@ module.exports = function (app) {
             for (let x = 0; x < checkme.length; x++) {
                 currDiff += checkme[i] - checkthem[i]
             }
-            console.log("currDiff",currDiff)
+            console.log("currDiff", currDiff)
             //If friends[i] has a lower score than theMatch, they are the new match
             if (currDiff > currWinner) {
                 theMatch = friends.friends[i];
                 console.log("THE BIGGEST ONE HERE IMPORTANT", theMatch)
             }
-                    //TODO: SHOW THEM THEIR MATCH
-
-        console.log("thematch",theMatch)
+            //TODO: SHOW THEM THEIR MATCH
+            console.log("thematch", theMatch)
         }
-        
+
         //push last so you don't friend yourself :(
         friends.friends.push(input)
         console.log("friends push confirm: ", friends)
